92d655238cf6b0c3f78d5b399479005cef8fbdfc
Racon94 <694543285@qq.com>
2020-09-15 19:47:08 +0800

    init

---
+++ q4findMedianSortedArrays.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// class Solution {
+// public:
+//     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+//     {
+//         return nums1[0];
+//     }
+// };
+
+int main()
+{
+    // Solution s;
+    // vector<int> nums1 = { 1, 3 };
+    // vector<int> nums2 = { 2 };
+
+    // cout << s.findMedianSortedArrays(nums1, nums2);
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
