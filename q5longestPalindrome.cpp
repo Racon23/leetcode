3fcc17532fd86459e61470db218c9b04fc999e29
Racon94 <694543285@qq.com>
2020-09-16 21:09:13 +0800

    q5

---
+++ q5longestPalindrome.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        
+        return s;
+    }
+};
+
+int main()
+{
+    Solution s;
+    string str = "babad";
+
+    cout << s.longestPalindrome(str);
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
