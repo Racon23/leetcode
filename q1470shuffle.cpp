9faf19c25317d415980906be548c061cd016714b
Racon94 <694543285@qq.com>
2020-09-18 00:06:05 +0800

    1

---
+++ q1470shuffle.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n)
+    {
+        
+        return NULL;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums = { 2, 5, 1, 3, 4, 7 };
+    int n = 3;
+
+    vector<int> r = s.shuffle(nums, n);
+
+    // cout << s.runningSum(nums).at(3);
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
