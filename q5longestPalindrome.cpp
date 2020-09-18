a747cb4718d5938c3c4734bc84b812398165a139
Racon94 <694543285@qq.com>
2020-09-18 17:39:19 +0800

    q5

--- q5longestPalindrome.cpp
+++ q5longestPalindrome.cpp
@@ -11,90 +11,28 @@ public:
     string longestPalindrome(string s)
     {
         int len = s.length();
-        string str = s.substr(len / 2, 1);
+        string str;
         string tstr;
         string tstr1;
         string tstr2;
         if (len <= 1)
             return s;
-        if (s.at(len / 2) == s.at(len / 2 + 1))
+        int i;
+        int ia;
+        if (len % 2 == 0)
         {
-            str = s.substr(len / 2, 2);
+            ia = len / 2 - 1;
         }
-        int i = len / 2;
-        int k = 1;
+        else
+        {
+            ia = len / 2;
+        }
+        str = s.substr(ia, 1);
+        int k = 0;
         int flag = 0;
         while (i >= 0 && i < len)
         {
-            i = len / 2 - k;
-            if (i >= 0)
-            {
-                for (int j = 1; j < len; j++)
-                {
-                    if (i - j >= 0 && i + j < len)
-                    {
-                        if (s.at(i - j) == s.at(i + j))
-                        {
-                            tstr = s.substr(i - j, j * 2 + 1);
-                            if (tstr.length() > str.length())
-                            {
-                                str = tstr;
-                                if (i - j == 0 || i + j == len - 1)
-                                {
-                                    flag = 1;
-                                    break;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-                if (i < len - 1 && s.at(i) == s.at(i + 1))
-                {
-                    tstr = s.substr(i, 2);
-                    if (tstr.length() > str.length())
-                    {
-                        str = tstr;
-                    }
-                    for (int j = 1; j < len; j++)
-                    {
-                        if (i - j >= 0 && i + j + 1 < len)
-                        {
-                            if (s.at(i - j) == s.at(i + j + 1))
-                            {
-                                tstr = s.substr(i - j, j * 2 + 2);
-                                if (tstr.length() > str.length())
-                                {
-                                    str = tstr;
-                                    if (i - j == 0 || i + j + 1 == len - 1)
-                                    {
-                                        flag = 1;
-                                        break;
-                                    }
-                                }
-                            }
-                            else
-                            {
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-            }
-            if (flag == 1)
-                return str;
-            i = len / 2 + k;
+            i = ia + k;
             if (i < len)
             {
                 for (int j = 1; j < len; j++)
@@ -159,9 +97,85 @@ public:
                         }
                     }
                 }
+                if (flag == 1)
+                    return str;
             }
-            if (flag == 1)
-                return str;
+
+            if (k == 0)
+            {
+                k++;
+                continue;
+            }
+
+            i = ia - k;
+            if (i >= 0)
+            {
+                for (int j = 1; j < len; j++)
+                {
+                    if (i - j >= 0 && i + j < len)
+                    {
+                        if (s.at(i - j) == s.at(i + j))
+                        {
+                            tstr = s.substr(i - j, j * 2 + 1);
+                            if (tstr.length() > str.length())
+                            {
+                                str = tstr;
+                                if (i - j == 0 || i + j == len - 1)
+                                {
+                                    flag = 1;
+                                    break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (i < len - 1 && s.at(i) == s.at(i + 1))
+                {
+                    tstr = s.substr(i, 2);
+                    if (tstr.length() > str.length())
+                    {
+                        str = tstr;
+                    }
+                    for (int j = 1; j < len; j++)
+                    {
+                        if (i - j >= 0 && i + j + 1 < len)
+                        {
+                            if (s.at(i - j) == s.at(i + j + 1))
+                            {
+                                tstr = s.substr(i - j, j * 2 + 2);
+                                if (tstr.length() > str.length())
+                                {
+                                    str = tstr;
+                                    if (i - j == 0 || i + j + 1 == len - 1)
+                                    {
+                                        flag = 1;
+                                        break;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (flag == 1)
+                    return str;
+            }
+
             k++;
         }
         return str;
@@ -171,18 +185,21 @@ public:
 int main()
 {
     Solution s;
-    // string str = "babad";
+    // string str = "ababd";
     // string str = "a";
     // string str = "aba";
     // string str = "abc";
     // string str = "abba";
-    string str = "abbbc";
+    // string str = "abbbc";
     // string str = "abccbd";
     // string str = "";
     // string str = "abcdadc";
     // string str = "abb";
     // string str = "ccd";
     // string str = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee";
+    // string str = "aaaa";
+    // string str = "aaaaa";
+    string str = "caba";
     clock_t time_start = clock();
     cout << s.longestPalindrome(str);
     cout << endl;
