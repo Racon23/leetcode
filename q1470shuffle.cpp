67e6da2f0142ea04171c8c5ac5517259de15b792
Racon94 <694543285@qq.com>
2020-09-18 08:29:10 +0800

    q1470

--- q1470shuffle.cpp
+++ q1470shuffle.cpp
@@ -8,8 +8,23 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n)
     {
-        
-        return NULL;
+        vector<int> nums1;
+        vector<int> nums2;
+        nums1.resize(n);
+        nums2.resize(n);
+        nums1.clear();
+        nums2.clear();
+        for (int i = 0; i < n; i++) {
+            nums1.push_back(nums.at(i));
+            nums2.push_back(nums.at(i + n));
+        }
+        nums.clear();
+        for (int i = 0; i < n; i++) {
+            nums.push_back(nums1.at(i));
+            nums.push_back(nums2.at(i));
+        }
+
+        return nums;
     }
 };
 
@@ -22,6 +37,9 @@ int main()
     vector<int> r = s.shuffle(nums, n);
 
     // cout << s.runningSum(nums).at(3);
+    for (int i = 0; i < nums.size(); i++) {
+        cout << nums.at(i) << " ";
+    }
     cout << endl;
 
     system("pause");
