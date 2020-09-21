aa3a1cda0b2896c737291edf5014866aab68723d
Racon94 <694543285@qq.com>
2020-09-21 18:54:41 +0800

    q8

--- q8myAtoi.cpp
+++ q8myAtoi.cpp
@@ -2,13 +2,50 @@
 #include <string>
 #include <vector>
 #include <ctime>
+#include <unordered_map>
 
 using namespace std;
 
-class Solution {
+class Automaton
+{
+    string state = "start";
+    // space +/- number other
+    unordered_map<string, vector<string>> table = {
+        {"start", {"start", "signed", "in_number", "end"}},
+        {"signed", {"end", "end", "in_number", "end"}},
+        {"in_number", {"end", "end", "in_number", "end"}},
+        {"end", {"end", "end", "end", "end"}}};
+
+    int get_col(char c)
+    {
+        if (isspace(c))
+            return 0;
+        if (c == '+' or c == '-')
+            return 1;
+        if (isdigit(c))
+            return 2;
+        return 3;
+    }
+
 public:
-    int myAtoi(string str) {
-        
+    int sign = 1;
+    long long ans = 0;
+
+    void get(char c) {
+        state = table[state][get_col(c)];
+        if()
+    }
+};
+
+class Solution
+{
+public:
+
+    int sign = 1;
+    long long ans =0;
+
+    int myAtoi(string str)
+    {
     }
 };
 
@@ -16,11 +53,11 @@ int main()
 {
     Solution s;
 
-    string str = "abc";
+    string str = "   -83723   ";
 
     clock_t time_start = clock();
     cout << s.myAtoi(str);
-    // cout << -32 % 10;
+
     cout << endl;
     clock_t time_end = clock();
     cout << "time use:" << 1000 * (time_end - time_start) / (double)CLOCKS_PER_SEC << "ms" << endl;
