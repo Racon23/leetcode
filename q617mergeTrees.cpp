f75906f418e749700a52f1b0e9894b1c2a103395
Racon94 <694543285@qq.com>
2020-09-23 18:35:39 +0800

    q617

--- q617mergeTrees.cpp
+++ q617mergeTrees.cpp
@@ -46,27 +46,15 @@ class Solution
 public:
     TreeNode *mergeTrees(TreeNode *t1, TreeNode *t2)
     {
-        if (t1 != nullptr && t2 != nullptr)
-        {
-            t1->val += t2->val;
-            t1->left = mergeTrees(t1->left, t2->left);
-            t1->right = mergeTrees(t1->right, t2->right);
-            return t1;
-        }
-        if (t1 != nullptr && t2 == nullptr)
-        {
-            t1->left = mergeTrees(t1->left, nullptr);
-            t1->right = mergeTrees(t1->right, nullptr);
-            return t1;
-        }
-        if (t1 == nullptr && t2 != nullptr)
-        {
-            t2->left = mergeTrees(nullptr, t2->left);
-            t2->right = mergeTrees(nullptr, t2->right);
+        if (t1 == nullptr)
             return t2;
-        }
+        if (t2 == nullptr)
+            return t1;
 
-        return nullptr;
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
     }
 };
 
